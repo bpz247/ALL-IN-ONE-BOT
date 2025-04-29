fc097cb793e63861c486f23f0c8578ede3746f66
bpz247 <falconamir01@gmail.com>
2025-04-30 00:45:08 +0600

    lavalink.js

--- lavalink.js
+++ lavalink.js
@@ -1,11 +1,10 @@
-
 module.exports = {
     enabled: true, 
     lavalink: {
-        identifier: 'GlaceYT',
-        password: 'glaceyt',
-        host: '193.226.78.187',
-        port: 9372,
+        identifier: 'bpz',
+        password: 'bpz',
+        host: 'meta.pylex.xyz',
+        port: 11302,
         secure: false
     }
 };
